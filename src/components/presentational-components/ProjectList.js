3c9b95a0240bb4a04eda72b10bab8ba05d3c50f4
Devin O'Leary <devin@Devins-MacBook-Pro.local>
2017-10-18 21:03:55 -0400

    added img-list overlays

--- src/components/presentational-components/ProjectList.js
+++ src/components/presentational-components/ProjectList.js
@@ -16,17 +16,17 @@ const ProjectList = (props) => {
     // mapping of projects
     const itemList = projectFilter.map(work => {
         return (
-          <li key={work.id}>
-            <div className="project_list_img-container">
-              <div className="project_list_img-overlay">
-                <h4 className="text-inverse">{work.acf.title}</h4>
+          <Link to="projectPane" smooth={true}>
+            <li key={work.id} onClick={props.isOpen.bind(this, work.id)}>
+              <div className="project_list_img-container">
+                <div className="project_list_img-overlay">
+                  <h4 className="text-inverse">{work.acf.title}</h4>
+                </div>      
+                <img src={work._embedded['wp:featuredmedia']["0"].source_url}
+                alt={work.slug}/>
               </div>
-              <Link to="projectPane" smooth={true}>
-              <img className="img-project_list" src={work._embedded['wp:featuredmedia']["0"].source_url}
-              alt={work.slug} onClick={props.isOpen.bind(this, work.id)}/>
-              </Link>
-            </div>
-          </li>
+            </li>
+          </Link>
         )
     })
     return (
